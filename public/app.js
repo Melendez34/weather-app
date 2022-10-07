1571e643e54ccf3e44a90e91075d1a13326e81d9
Melendez34 <omar.mel.lopez@gmail.com>
2022-10-07 13:12:41 -0500

    Ajustment on method and frontend post data

--- public/app.js
+++ public/app.js
@@ -1,10 +1,33 @@
 let lat, lon
 const button = document.getElementById('geolocationBtn')
-button.addEventListener('click', async (event) => {
+button.addEventListener('click', (event) => {
+
   const data = {
-    latitude: lat,
-    longitude: lon,
+    channelid: 50,
+    config: {
+      facebook_client_app_id: '710342110221387',
+      facebook_client_id: "undefined",
+      facebook_secret_app_id: "285be05a9ac59460a993e4510dc5bb88",
+      facebook_secret_id: "undefined",
+      google_client_app_id : "249146836846-e5ltjjrillcp73an36cbe01ip89c6sih.apps.googleusercontent.com",
+      google_secret_id: "undefined",
+      id_menu: 'pixelwarrior',
+      login: true,
+      login_anonymous: true,
+      url_terminos: 'https://pixelwarrior.com.mx/trinity/privacy.txt'
+    },
+    status: true,
+    id: 12312,
+    url: 'http://pixelwarrior.com.mx',
+    namepartner: 'PixelWarrior Testing Area - DEV',
+    nameskey: 'pixelwarrior',
+    created_at: 1663202142833,
+    customJS: "const dato = 3.1416",
+    customCSS: "body { color: #000cc0 }",
+    updated_at: 1664326669046,
   }
+  document.getElementById('data-partner').textContent = JSON.stringify(data)
+
   const options = {
     method: 'POST',
     headers: {
@@ -13,9 +36,9 @@ button.addEventListener('click', async (event) => {
     body: JSON.stringify(data)
   }
 
-  const response = await fetch('/api', options)
-  const json = await response.json().catch(error => { console.log(error) })
-  console.log('mandando lat y long: ',json);
+  fetch('/first-partner', options)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
 })
 
 if ('geolocation' in navigator) {
@@ -30,51 +53,32 @@ if ('geolocation' in navigator) {
         document.getElementById('longitude').textContent = lon.toFixed(2)
   
         const url_api_weather = `weather/${lat},${lon}`
-        //const url_api_weather = `/weather`
         const response = await fetch(url_api_weather)
         const json = await response.json()
         console.log(json)
-      } catch (error) {
-        console.error(error);
-      }
 
+        const data = {
+          latitude: lat,
+          longitude: lon,
+        }
+
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }
+
+        const post_response = await fetch('/api', options)
+        const post_json = await post_response.json()
+        console.log('mandando lat y long: ', post_json)
+
+      } catch (error) {
+        console.error(error)
+      }
       
     })
 } else {
   console.log('geolocation not available');
 }
-
-const data = {
-  channelid: 50,
-  config: {
-    facebook_client_app_id: '710342110221387',
-    facebook_client_id: "undefined",
-    facebook_secret_app_id: "285be05a9ac59460a993e4510dc5bb88",
-    facebook_secret_id: "undefined",
-    google_client_app_id : "249146836846-e5ltjjrillcp73an36cbe01ip89c6sih.apps.googleusercontent.com",
-    google_secret_id: "undefined",
-    id_menu: 'pixelwarrior',
-    login: true,
-    login_anonymous: true,
-    url_terminos: 'https://pixelwarrior.com.mx/trinity/privacy.txt'
-  },
-  status: true,
-  id: 12312,
-  url: 'http://pixelwarrior.com.mx',
-  namepartner: 'PixelWarrior Testing Area - DEV',
-  nameskey: 'pixelwarrior',
-  created_at: 1663202142833,
-  customJS: "",
-  updated_at: 1664326669046,
-}
-
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
-}
-fetch('/first-partner', options)
-.then((response) => response.json())
-.then((data) => console.log(data))
